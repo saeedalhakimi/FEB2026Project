cf7885173c90cd4f4d02704181553a050f7b05b3
Saeed Al hakimi <Saeed.alhakimi@live.com>
2026-02-05 11:05:16 +0300

    Add project files.

---
+++ DOT10.Business/Application/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EXOBusiness.Application.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationRegistrationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
